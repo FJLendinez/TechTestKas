cf4449e268a3fba50a521ee3d3318883742e8d67
Francisco Javier Lendínez Tirado <fjlendinez@outlook.es>
2017-12-17 13:17:57 +0100

    Update models.py

--- TechTestKasProject/API/models.py
+++ TechTestKasProject/API/models.py
@@ -10,6 +10,8 @@ class TimeStampedModel(models.Model):
     """
     created = models.DateTimeField(auto_now=True)
     modified = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        abstract = True
 
 
 class Article(TimeStampedModel):
